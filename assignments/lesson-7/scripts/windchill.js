e8ac63268ead2d7076d99cfdade81f459b7812bd
Casey-Coleman <yesacnameloc@gmail.com>
2019-03-05 19:59:54 -0700

    lesson 7

--- assignments/lesson-7/scripts/windchill.js
+++ assignments/lesson-7/scripts/windchill.js
@@ -1,6 +1,6 @@
-var temp = document.getElementById("temp").innerHTML; //temperature
-var windSpeed = 10; //windSpeed
-var windChill = 35.74 + 0.6215 * temp - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275* t * Math.pow(windSpeed, 0.16);
+var temp = 50; 
+var windSpeed = 10; 
+var windChill = 35.74 + 0.6215 * temp - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temp * Math.pow(windSpeed, 0.16);
 
 document.getElementById("temp").innerHTML = temp + '&deg';
 document.getElementById("windSpeed").innerHTML = windSpeed + ' mph';
